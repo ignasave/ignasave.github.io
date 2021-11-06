397c26bfc6913504afdd801bc6407a2a3e5afe64
ignasave <vazquez_ignacio_martin@hotmail.com>
2021-11-06 20:34:21 -0300

    terminado esqueleto de sesktop

--- js/index.js
+++ js/index.js
@@ -1,12 +1,36 @@
 /* -------------- Click Handlers -------------- */
 
 const onClickMobileSideButton = () => {
-    const sideButtons = document.getElementsByClassName('mobile-side-buttons')[0]
-    const classes = sideButtons.classList
-    
-    if(classes.contains('open')){
-        classes.remove('open')
-    } else {
-        classes.add('open')
-    }
+	const sideButtons = document.getElementsByClassName('mobile-side-buttons')[0];
+	const classes = sideButtons.classList;
+
+	if (classes.contains('open')) {
+		classes.remove('open');
+	} else {
+		classes.add('open');
+	}
+};
+const onClickMobileSideSelectorButton = () => {
+	const sideButtons = document.getElementsByClassName('selector-content')[0];
+	const classes = sideButtons.classList;
+
+	if (classes.contains('open')) {
+		classes.remove('open');
+	} else {
+		classes.add('open');
+	}
+};
+
+/* -------------- Proyects -------------- */
+
+const onChangeProject = (projectID) => {
+	const project = document.getElementById(projectID);
+	const projects = document.getElementsByClassName('proyect');
+
+	for (let i = 0; i < projects.length; i++) {
+		projects[i].classList.remove('selected');
+	}
+
+	project.classList.add('selected');
+	selectedProjectID = projectID;
 }
